1bd7e47d2e00715a4c72fa15b97c35405f559fd5
yogeshvj1912 <113254846+yogeshvj1912@users.noreply.github.com>
2025-01-06 09:43:55 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Change to '/subpath/' if hosted on a subpath
+});
+
